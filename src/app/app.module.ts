20449868b7e9abf4b434edba3c6dd279271e6c16
wannesds <wannesdieltiens@gmail.com>
2019-11-05 10:41:47 +0100

    First commit from terminal, created the basic site structure, horizon nav, tryout logo, init components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    WorkComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
