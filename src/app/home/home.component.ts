9188d09786cdc7ebc235eb3a6ca62dad237cdeef
wannesds <wannesdieltiens@gmail.com>
2021-01-18 17:09:49 +0100

    fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component} from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
 
     
 
